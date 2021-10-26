86a31fe680d73f2534d5012748ab20cd02703292
Katerina5005 <kat51082@gmail.com>
2021-10-26 23:35:02 +0600

    js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 let name = "Katerin"
 const data = 26_10_2021
 
-console.log (name, data)
+const nav = document.querySelector (".nav")
+const item = document.querySelectorAll (".item")
+const batn = document.querySelector ( ".btn")
+
+console.log ( typeof nav, typeof item)
+
+for (let navItem of item) {
+    navItem.addEventListener ("click", function () {
+        console.log ('link clicked');
+})
+}
+
+batn.addEventListener ( "click", function () {
+    console.log ( 'btn linked');
+})
